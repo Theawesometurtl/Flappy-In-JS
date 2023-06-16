846bec890839f189d74abdca09fed4d7e0a8bcef
Theawesometurtl <shablammmo@gmail.com>
2023-06-15 17:26:48 -0700

    updated a content hash

--- webpack.prod.js
+++ webpack.prod.js
@@ -12,7 +12,7 @@ module.exports = merge(common, {
   },
   mode: "production",
   output: {
-    filename: "[name].[contentHash].bundle.js",
+    filename: "[name].[hash].bundle.js",
     path: path.resolve(__dirname, "docs")
   },
   optimization: {
